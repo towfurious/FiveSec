d7178d609f9292b61628c6009c6cf00de912e735
Viktor Shavarin <towfurious@gmail.com>
2016-03-29 18:01:46 +0300

    Create MainActivity class and layout

---
+++ app/src/main/java/com/shavarin/viktor/fivesec/MainActivity.java
@@ -0,0 +1,16 @@
+package com.shavarin.viktor.fivesec;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by viktorshavarin on 3/29/16.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
