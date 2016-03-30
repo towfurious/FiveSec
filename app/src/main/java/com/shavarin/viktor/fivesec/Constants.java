4e38a929c38df03ccf0ff6f525da659f8042fbf5
Viktor Shavarin <towfurious@gmail.com>
2016-03-31 02:11:25 +0300

    [FS-6] Implemented Navigation View and Tab navigation with test fragments

---
+++ app/src/main/java/com/shavarin/viktor/fivesec/Constants.java
@@ -0,0 +1,8 @@
+package com.shavarin.viktor.fivesec;
+
+
+public class Constants {
+    public static final int TAB_ONE = 0;
+    public static final int TAB_TWO = 1;
+    public static final int TAB_THREE = 2;
+}
---
+++ app/src/main/java/com/shavarin/viktor/fivesec/fragment/ExampleFragment.java
@@ -0,0 +1,30 @@
+package com.shavarin.viktor.fivesec.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.shavarin.viktor.fivesec.R;
+
+public class ExampleFragment extends Fragment {
+    private static final int LAYOUT = R.layout.fragment_example;
+
+    private View view;
+
+    public static ExampleFragment getInstance() {
+        Bundle args = new Bundle();
+        ExampleFragment fragment = new ExampleFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(LAYOUT, container, false);
+        return view;
+    }
+}
