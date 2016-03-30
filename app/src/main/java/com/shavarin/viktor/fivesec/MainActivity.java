2ee8de3a7587724013f74522166a6770e12aacb5
Viktor Shavarin <towfurious@gmail.com>
2016-03-30 16:43:42 +0300

    [FS-3] Added Toolbar and search button and icon

--- app/src/main/java/com/shavarin/viktor/fivesec/MainActivity.java
+++ app/src/main/java/com/shavarin/viktor/fivesec/MainActivity.java
@@ -2,15 +2,35 @@ package com.shavarin.viktor.fivesec;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Created by viktorshavarin on 3/29/16.
  */
+
 public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
